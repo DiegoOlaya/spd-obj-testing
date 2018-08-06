ca2fd270001091bb3b7cf9379e4216123cccd928
Diego Olaya <ogeidayalo@gmail.com>
2018-08-06 17:14:12 -0600

    Implement class methods.

--- src/app/complex-num.ts
+++ src/app/complex-num.ts
@@ -22,11 +22,25 @@ export class ComplexNum {
   }
 
   /**
+    Return the real part of the complex number.
+  */
+  public getReal() {
+    return this.real;
+  }
+
+  /**
     Change the imaginary number to user provided value.
     @arg newImaginary Type: number. The value to change the imaginary portion to.
   */
   public setImaginary(newImaginary:number) {
     this.imaginary = newImaginary;
+  }
+
+  /**
+    Return the imaginary part of the complex number.
+  */
+  public getImaginary() {
+    return this.imaginary;
   }
 
   /**
@@ -39,8 +53,53 @@ export class ComplexNum {
     this.setImaginary(newImaginary);
   }
 
+  /**
+    Multiply this complex number by another complex number.
+    @WARNING: This function WILL OVERWRITE THE CURRENT VALUE of this object with
+    the result of the computation.
+    @arg cNum Type: ComplexNum. The complex number this will be multiplied by.
+  */
   public multiply(cNum:ComplexNum) {
-    
+    var newReal = (this.real * cNum.getReal()) - (this.imaginary * cNum.getImaginary());
+    var newImag = (this.real * cNum.getImaginary()) + (this.imaginary * cNum.getReal());
+    this.setValues(newReal, newImag);
+  }
+
+  /**
+    Divide this complex number by another complex number.
+    @WARNING: This function WILL OVERWRITE THE CURRENT VALUE of this object with
+    the result of the computation.
+    @arg cNum Type: ComplexNum. The complex number this will be divided by.
+  */
+  public divide(cNum:ComplexNum) {
+    var divisor = Math.pow(cNum.getReal(), 2) + Math.pow(cNum.getImaginary(), 2);
+    var newReal = ((this.real * cNum.getReal()) + (this.imaginary * cNum.getImaginary())) / divisor;
+    var newImag = ((this.imaginary * cNum.getReal()) - (this.real * cNum.getImaginary())) / divisor;
+    this.setValues(newReal, newImag);
+  }
+
+  /**
+    Add this complex number to another complex number.
+    @WARNING: This function WILL OVERWRITE THE CURRENT VALUE of this object with
+    the result of the computation.
+    @arg cNum Type: ComplexNum. The complex number to be added to this.
+  */
+  public add(cNum:ComplexNum) {
+    var newReal = this.real + cNum.getReal();
+    var newImag = this.imaginary + cNum.getImaginary();
+    this.setValues(newReal, newImag);
+  }
+
+  /**
+    Take the square root of this number.
+    @WARNING: This function WILL OVERWRITE THE CURRENT VALUE of this object with
+    the result of the computation.
+  */
+  public sqrt() {
+    var realNum = this.real + Math.sqrt(Math.pow(this.real, 2) + Math.pow(this.imaginary, 2));
+    var real = realNum / Math.sqrt(2 * realNum);
+    var imag = this.imaginary / Math.sqrt(2 * realNum);
+    this.setValues(real, imag);
   }
 
 }

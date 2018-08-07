736b0a456039b5c2a328f138708f92fa15301900
Diego Olaya <ogeidayalo@gmail.com>
2018-08-06 18:07:20 -0600

    Add generateComplexNums() function.

--- src/app/object-time/object-time.component.ts
+++ src/app/object-time/object-time.component.ts
@@ -8,7 +8,22 @@ import { ComplexNum } from './complex-num';
 })
 export class ObjectTimeComponent implements OnInit {
 
+  private complexNums:ComplexNum[];
+  public randRange:number = 51;
+
   constructor() { }
+
+  /**
+    Generate 10 complex numbers for use in timing function.
+  */
+  private generateComplexNums() {
+    this.complexNums = [];
+    for (var i = 0; i < 10; i++) {
+      this.complexNums.push(
+        new ComplexNum(Math.random() * this.randRange, Math.random() * this.randRange)
+      );
+    }
+  }
 
   ngOnInit() {
   }

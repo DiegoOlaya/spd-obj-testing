e801ef5bcaec5fc96e2b14a238aa107f706fdddb
Diego Olaya <ogeidayalo@gmail.com>
2018-08-15 16:50:01 -0600

    Implement code to confirm both implementations agree.

--- src/app/test-accuracy/test-accuracy.component.ts
+++ src/app/test-accuracy/test-accuracy.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ObjectTimeComponent } from '../object-time/object-time.component';
+import { NumberTimeComponent } from '../number-time/number-time.component';
 
 @Component({
   selector: 'app-test-accuracy',
   templateUrl: './test-accuracy.component.html',
   styleUrls: ['./test-accuracy.component.css']
 })
+
+//Allows access to child object methods.
+@ViewChild(ObjectTimeComponent)
+@ViewChild(NumberTimeComponent)
+
 export class TestAccuracyComponent implements OnInit {
+
+  public randNums:number[];
+  public randRange = 51;
+  //Store result of computation.
+  public objResult:number[] = [];
+  public numResult:number[] = [];
 
   constructor() { }
 
+  public generateNums(range:number) {
+    this.randNums = [];
+    for (var i = 0; i < 20; i++) {
+      this.randNums.push(Math.random() * range);
+    }
+  }
+
+  public compute() {
+    var objComp = new ObjectTimeComponent();
+    var numComp = new NumberTimeComponent();
+    numComp.timeSingle(this.randNums);
+    objComp.startTiming(1, this.randNums);
+    this.objResult = [objComp.compResult.getReal(), objComp.compResult.getImaginary()];
+    this.numResult = [numComp.resultR, numComp.resultI];
+  }
+
   ngOnInit() {
+    this.generateNums(this.randRange);
   }
 
 }
